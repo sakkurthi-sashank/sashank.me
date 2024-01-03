dd46faa66cd068e5d76d6ed17e96ab96f64ad2d3
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-01-03 15:53:42 +0530

    [feat] deploy github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    loader: "akamai",
     domains: ["github.com", "encrypted-tbn0.gstatic.com"],
+    path: "",
   },
+  assetPrefix: "./",
 };
 
 module.exports = nextConfig;
