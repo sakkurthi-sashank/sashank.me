9d2a99c6b0f39e9f968e173cebc9692496e5975b
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-01-03 21:09:10 +0530

    [feat] vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
