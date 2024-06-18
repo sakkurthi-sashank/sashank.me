02eb1f4e985b86cf63926c8bf928af2fbe3ab664
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-06-18 11:11:54 +0530

    fix: spacing updated

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -76,7 +76,7 @@ export function Projects() {
     >
       <div className="z-10 my-auto">
         <PageTitle title="Projects" />
-        <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {projects.map((project, index) => (
             <div
               key={index}
